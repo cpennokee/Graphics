f48dcdeae38f337040ae257a50537ef3d533d32e
Christian Pennokee <cpennokee@ou.edu>
2018-11-14 14:27:40 -0600

    Add a line

--- src/MyFrame.java
+++ src/MyFrame.java
@@ -1,5 +1,29 @@
+import javax.swing.JFrame;
 
-public class MyFrame
+public class MyFrame extends JFrame
 {
-    private MyPanel
+    private final int WIDTH = 300;
+    private MyPanel panel;
+    
+    public MyFrame(String title)
+    {
+        setTitle(title);
+        setSize(WIDTH, 400);
+        setLocationRelativeTo(null);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setVisible(true);
+        
+    }
+    
+    public void paintLine()
+    {
+        panel = new LineDemoPanel();
+        add(panel);
+        setVisible(true);
+    }
+    public static void main(String[] args)
+    {
+        MyFrame frame = new MyFrame("Hello");
+        frame.paintLine();
+    }
 }
